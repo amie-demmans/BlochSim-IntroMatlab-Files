b24076ee26b9131a390efd07a75ca61c1160bd40
amie-demmans <154541326+amie-demmans@users.noreply.github.com>
2024-02-22 15:31:44 -0500

    Delete test directory

--- test/srsignal.m
+++
@@ -1,8 +0,0 @@
-function Mss = srsignal(flip,T1,T2,TR,df)
-
-Rflip = yrot(flip); %flip matrix w rotation along yaxis
-[Atr,Btr] = freeprecess(TR,T1,T2,df); %free precession matrix for time = TR
-
-Atr = [0,0,0;0,0,0;0,0,1]*Atr; % define new matrix Atr for steady state magentization neglecting transverse magnetization
-
-Mss = (inv(eye(3)-Atr*Rflip)*Btr); % apply steady state magnetization with new Atr to neglect transverse magnetization
